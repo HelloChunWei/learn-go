47783921029bbf76ae5f52317df5e81dec4d1b3d
Neil Kuo <kissme3131@gmail.com>
2026-04-05 23:27:55 +0800

    2026/04/05

---
+++ internal/headers/hearders.go
@@ -0,0 +1,83 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type Headers struct {
+	headers map[string]string
+}
+
+var SEPARATOR = []byte("\r\n")
+
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
+}
+
+func (h *Headers) get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h *Headers) set(name, value string) {
+	h.headers[strings.ToLower(name)] = value
+}
+
+var ERROR_BAD_HEADER = fmt.Errorf("bad header")
+var ERROR_BAD_KEY = fmt.Errorf("bad filed name")
+
+func isValidateKey(str string) bool {
+	// 只允許 A-Z, a-z, 0-9 和特定特殊字元
+	pattern := `^[A-Za-z0-9!#$%&'*+\-.^_` + "`" + `|~]+$`
+	matched, _ := regexp.MatchString(pattern, str)
+	return matched
+}
+
+func parseHeader(data []byte) (string, string, error) {
+	parts := bytes.SplitN(data, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", ERROR_BAD_HEADER
+	}
+	if bytes.HasSuffix(parts[0], []byte(" ")) {
+		return "", "", ERROR_BAD_HEADER
+	}
+
+	strKey := string(parts[0])
+	if !isValidateKey(strKey) {
+		return "", "", ERROR_BAD_KEY
+	}
+	strValue := string(bytes.TrimSpace(parts[1]))
+	return strings.ToLower(strKey), strValue, nil
+
+}
+
+func (h *Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+
+	for {
+		idx := bytes.Index(data[read:], SEPARATOR)
+		fmt.Println(idx)
+		if idx == -1 {
+			break
+		}
+		// empty header
+		if idx == 0 {
+			done = true
+			read += len(SEPARATOR)
+			break
+		}
+		key, value, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return 0, false, err
+		}
+		read += idx + len(SEPARATOR)
+		h.set(key, value)
+	}
+	return read, done, nil
+
+}
---
+++ internal/headers/hearders_test.go
@@ -0,0 +1,36 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHearderparse(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers.get("Host"))
+	assert.Equal(t, "", headers.get("dasdsad"))
+	assert.Equal(t, 25, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
